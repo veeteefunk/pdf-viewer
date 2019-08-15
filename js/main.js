7dc66461a1898745043eb4d6c770069d34eb20cf
Victor Marin <victormarinviloria@gmail.com>
2019-08-15 20:44:10 +0200

    Add files via upload
    3rd commit

--- js/main.js
+++ js/main.js
@@ -65,22 +65,24 @@ const showNextPage = () => {
 };
 
 // Get Document
-pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
-  pdfDoc = pdfDoc_;
+pdfjsLib
+  .getDocument(url)
+  .promise.then(pdfDoc_ => {
+    pdfDoc = pdfDoc_;
 
-  document.querySelector("#page-count").textContent = pdfDoc.numPages;
+    document.querySelector("#page-count").textContent = pdfDoc.numPages;
 
-  renderPage(pageNum);
-});
-//   .catch(err => {
-//     // Display error
-//     const div = document.createElement("div");
-//     div.className = "error";
-//     div.appendChild(document.createTextNode(err.message));
-//     document.querySelector("body").insertBefore(div, canvas);
-//     // Remove top bar
-//     document.querySelector(".top-bar").style.display = "none";
-//   });
+    renderPage(pageNum);
+  })
+  .catch(err => {
+    // Display error
+    const div = document.createElement("div");
+    div.className = "error";
+    div.appendChild(document.createTextNode(err.message));
+    document.querySelector("body").insertBefore(div, canvas);
+    // Remove top bar
+    document.querySelector(".top-bar").style.display = "none";
+  });
 
 // Button Events
 document.querySelector("#prev-page").addEventListener("click", showPrevPage);
